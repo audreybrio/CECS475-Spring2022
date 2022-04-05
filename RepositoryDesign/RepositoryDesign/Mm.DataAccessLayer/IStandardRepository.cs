e3e0ab36e284480e034046879cb2deed507104fd
audreybrio <49995869+audreybrio@users.noreply.github.com>
2022-04-05 16:54:13 -0700

    chanes

---
+++ RepositoryDesign/RepositoryDesign/Mm.DataAccessLayer/IStandardRepository.cs
@@ -0,0 +1,14 @@
+﻿using DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mm.DataAccessLayer
+{
+    public interface IStandardRepository : IRepository<Standard>
+    {
+
+    }
+}
---
+++ RepositoryDesign/RepositoryDesign/Mm.DataAccessLayer/StudentRepository.cs
@@ -0,0 +1,18 @@
+﻿using DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mm.DataAccessLayer
+{
+    public class StudentRepository : Repository<Student>, IStudentRepository
+    {
+
+        public StudentRepository(): base(new SchoolDBEntities())
+        {
+        }
+
+    }
+}
